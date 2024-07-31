51f88661dd8a4f8ff0bf765ec4a171fa0515d580
Osman Cengeri <47080303+VS-57@users.noreply.github.com>
2024-07-31 16:19:48 +0300

    First Commit

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import gameGarajRouter from "./routes/gameGaraj.mjs";
+import gamingGenRouter from "./routes/gamingGen.mjs";
+import itopyaRouter from "./routes/itopya.mjs";
+import pckolikRouter from "./routes/pckolik.mjs";
+import vatanRouter from "./routes/vatan.mjs";
+import setupSwagger from "./swagger/swagger.mjs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/api/game-garaj", gameGarajRouter);
+app.use("/api/gaming-gen", gamingGenRouter);
+app.use("/api/itopya", itopyaRouter);
+app.use("/api/pckolik", pckolikRouter);
+app.use("/api/vatan", vatanRouter);
+
+// Swagger dokümantasyonu için setup
+setupSwagger(app);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
