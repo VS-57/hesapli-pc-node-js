2570ff291aa061e47613cfbb2dbfe3af2f12a5ba
Cengeriagro <cengeriailesi@hotmail.com>
2024-08-06 23:53:33 +0300

    Update inceHesap.mjs

--- routes/inceHesap.mjs
+++ routes/inceHesap.mjs
@@ -4,68 +4,107 @@ import { JSDOM } from "jsdom";
 
 const router = express.Router();
 
+async function fetchPageData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error(`Error fetching page ${url}: ${response.statusText}`);
+    const text = await response.text();
+    const dom = new JSDOM(text);
+    return dom.window.document;
+  } catch (error) {
+    throw new Error(`Failed to fetch page ${url}: ${error.message}`);
+  }
+}
+
+async function getTotalPages(baseUrl) {
+  try {
+    const doc = await fetchPageData(baseUrl);
+    const links = doc.querySelectorAll("nav a");
+
+    const secondLastLink = links[links.length - 2];
+
+    const secondLastLinkText = secondLastLink.textContent.trim();
+    return secondLastLinkText ? parseInt(secondLastLinkText, 10) : 1;
+  } catch (error) {
+    throw new Error(`Failed to fetch total pages: ${error.message}`);
+  }
+}
+
 async function fetchAllProducts(urls) {
   const products = [];
+
   const fetchPromises = urls.map(async (url) => {
-    const response = await fetch(url);
-    const html = await response.text();
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
-    const productElements = doc.querySelectorAll(
-      ".grid.grid-cols-2.md\\:grid-cols-3.gap-1 > div"
-    );
-
-    productElements.forEach((productElement) => {
-      const linkElement = productElement.querySelector("a[itemprop='url']");
-      const imageElement = productElement.querySelector("img");
-      const nameElement = productElement.querySelector(
-        "p.text-lg.text-center.truncate.font-semibold[title]"
+    try {
+      const response = await fetch(url);
+      const html = await response.text();
+      const dom = new JSDOM(html);
+      const doc = dom.window.document;
+      const productElements = doc.querySelectorAll(
+        ".grid.grid-cols-2.md\\:grid-cols-3.gap-1 > div"
       );
-      const priceElement = productElement.querySelector(".text-orange-500");
 
-      const link =
-        "https://www.incehesap.com" + linkElement.getAttribute("href");
-      const name = nameElement ? nameElement.textContent.trim() : "No name";
+      productElements.forEach((productElement) => {
+        const linkElement = productElement.querySelector("a[itemprop='url']");
+        const imageElement = productElement.querySelector("img");
+        const nameElement = productElement.querySelector(
+          "p.text-lg.text-center.truncate.font-semibold[title]"
+        );
+        const priceElement = productElement.querySelector(".text-orange-500");
 
-      // Fiyatı al ve sayıya dönüştür
-      const priceText = priceElement
-        ? priceElement.textContent.trim().replace(/\s+/g, " ")
-        : "0";
-      const price =
-        parseFloat(priceText.replace(/[^\d,]/g, "").replace(",", ".")) || 0;
+        const link =
+          "https://www.incehesap.com" + linkElement.getAttribute("href");
+        const name = nameElement ? nameElement.textContent.trim() : "No name";
 
-      const image = imageElement
-        ? "https://www.incehesap.com" + imageElement.getAttribute("src")
-        : "No image";
+        // Fiyatı al ve sayıya dönüştür
+        const priceText = priceElement
+          ? priceElement.textContent.trim().replace(/\s+/g, " ")
+          : "0";
+        const price =
+          parseFloat(priceText.replace(/[^\d,]/g, "").replace(",", ".")) || 0;
 
-      const specs = {};
+        const image = imageElement
+          ? "https://www.incehesap.com" + imageElement.getAttribute("src")
+          : "No image";
 
-      productElement.querySelectorAll("ul li").forEach((specElement) => {
-        const specText = specElement.textContent.trim();
+        const specs = {};
 
-        // Belirli anahtarlar ile eşleştirme yap
-        if (
-          specText.includes("AMD") ||
-          (specText.includes("Intel") &&
-            !specText.toLowerCase().includes("arc"))
-        ) {
-          specs["CPU"] = specText;
-        } else if (
-          specText?.toLowerCase().includes("rtx") ||
-          specText?.toLowerCase().includes("rx") ||
-          specText?.toLowerCase().includes("gtx") ||
-          specText?.toLowerCase().includes("arc")
-        ) {
-          specs["GPU"] = specText;
-        } else if (specText.includes("DDR4") || specText.includes("DDR5")) {
-          specs["Ram"] = specText;
-        } else if (specText.includes("SSD")) {
-          specs["Storage"] = specText;
-        }
+        productElement.querySelectorAll("ul li").forEach((specElement) => {
+          const specText = specElement.textContent.trim();
+
+          // Belirli anahtarlar ile eşleştirme yap
+          if (
+            specText.includes("AMD") ||
+            (specText.includes("Intel") &&
+              !specText.toLowerCase().includes("arc"))
+          ) {
+            specs["CPU"] = specText;
+          } else if (
+            specText?.toLowerCase().includes("rtx") ||
+            specText?.toLowerCase().includes("rx") ||
+            specText?.toLowerCase().includes("gtx") ||
+            specText?.toLowerCase().includes("arc")
+          ) {
+            specs["GPU"] = specText;
+          } else if (specText.includes("DDR4") || specText.includes("DDR5")) {
+            specs["Ram"] = specText;
+          } else if (specText.includes("SSD")) {
+            specs["Storage"] = specText.replace("Hazır Sistem", "");
+          }
+        });
+
+        products.push({
+          name,
+          price,
+          image,
+          link,
+          specs,
+          store: "inceHesap",
+        });
       });
-
-      products.push({ name, price, image, link, specs, store: "inceHesap" });
-    });
+    } catch (error) {
+      console.error("Error fetching URL:", url, error);
+    }
   });
 
   await Promise.all(fetchPromises);
@@ -83,9 +122,10 @@ function generateUrls(base, totalPages) {
 
 router.get("/", async (req, res) => {
   const baseUrl = "https://www.incehesap.com/hazir-sistemler-fiyatlari/";
-  const totalPages = 14; // Örnek olarak toplam 14 sayfa olduğu varsayıldı
-  const urls = generateUrls(baseUrl, totalPages);
   try {
+    const totalPages = await getTotalPages(baseUrl);
+    console.log(totalPages);
+    const urls = generateUrls(baseUrl, totalPages);
     const products = await fetchAllProducts(urls);
     res.json(products);
   } catch (error) {
