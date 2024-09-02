cff7670308e7052386a9033b0b66534f143852fa
Cengeriagro <cengeriailesi@hotmail.com>
2024-09-02 19:22:44 +0300

    proxyy

--- routes/vatan.mjs
+++ routes/vatan.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { MongoClient } from "mongodb";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 puppeteer.use(StealthPlugin());
 
@@ -10,6 +12,7 @@ const router = express.Router();
 const proxyAddress = "216.173.84.218:6133"; // Proxy IP and port
 const proxyUsername = "KE4rXkUJ"; // Proxy username
 const proxyPassword = "KE4rXkUJ"; // Proxy password
+const apikey = "6f0992eec3d5c03b0a10ca458eb4c434b6e4cc37"; // ZenRows API anahtarı
 
 // MongoDB connection details
 const mongoUrl =
@@ -17,88 +20,114 @@ const mongoUrl =
 const dbName = "ucuzasistem";
 const collectionName = "vatan"; // Collection name set to "vatan"
 
-async function getTotalPages(page, url) {
-  await page.goto(url, { waitUntil: "load", timeout: 0 });
+async function getTotalPages(url) {
+  try {
+    const response = await axios({
+      url: "https://api.zenrows.com/v1/",
+      method: "GET",
+      params: {
+        url: url,
+        apikey: apikey,
+      },
+    });
 
-  const totalPages = await page.evaluate(() => {
-    const paginationItems = document.querySelectorAll(".pagination__item");
-    if (paginationItems.length < 2) {
-      return 1;
-    }
+    // DOM'u parçalayıcı bir kütüphane olan cheerio'yu kullanarak HTML'den elemanları çekiyoruz
+    const cheerio = require("cheerio");
+    const $ = cheerio.load(response.data);
 
-    const secondToLastItem = paginationItems[paginationItems.length - 2];
-    return parseInt(secondToLastItem.textContent.trim(), 10) || 1;
-  });
+    const paginationItems = $(".pagination__item");
 
-  return totalPages;
+    const secondToLastItem = paginationItems
+      .eq(paginationItems.length - 2)
+      .text();
+    console.log(secondToLastItem);
+    return parseInt(secondToLastItem.replace(/[^\d]/g, ""), 10) || 1;
+  } catch (error) {
+    console.error("Error fetching total pages:", error);
+    return 1; // Hata durumunda 1 sayfa varsayıyoruz
+  }
 }
 
-async function fetchAllProducts(page, urls) {
+async function fetchAllProducts(urls) {
   const products = [];
 
   for (const url of urls) {
-    await page.goto(url, { waitUntil: "load", timeout: 0 });
+    try {
+      const response = await axios({
+        url: "https://api.zenrows.com/v1/",
+        method: "GET",
+        params: {
+          url: url,
+          apikey: apikey,
+          js_render: "true",
+          premium_proxy: "true",
+        },
+      });
 
-    const productsOnPage = await page.evaluate(() => {
-      const productElements = document.querySelectorAll(
+      const $ = cheerio.load(response.data); // HTML yanıtını cheerio ile yüklüyoruz
+
+      const productElements = $(
         ".product-list.product-list--list-page .product-list-link"
       );
 
-      return Array.from(productElements).map((productElement) => {
-        const nameElement = productElement.querySelector(
-          ".product-list__product-name h3"
-        );
-        const priceElement = productElement.querySelector(
-          ".product-list__price"
-        );
-        const imageElement = productElement.querySelector(
-          ".product-list__image-safe img"
-        );
+      const productsOnPage = Array.from(productElements).map(
+        (productElement) => {
+          const nameElement = $(productElement).find(
+            ".product-list__product-name h3"
+          );
+          const priceElement = $(productElement).find(".product-list__price");
+          const imageElement = $(productElement).find(
+            ".product-list__image-safe img"
+          );
 
-        const link =
-          "https://www.vatanbilgisayar.com" +
-          productElement.getAttribute("href");
-        const name = nameElement ? nameElement.textContent.trim() : "No name";
+          const link =
+            "https://www.vatanbilgisayar.com" + $(productElement).attr("href");
+          const name = nameElement.length
+            ? nameElement.text().trim()
+            : "No name";
 
-        const priceText = priceElement
-          ? priceElement.textContent.trim().replace(/\s+/g, " ")
-          : "0";
-        const price =
-          parseFloat(priceText.replace(/[^\d,]/g, "").replace(",", ".")) || 0;
+          const priceText = priceElement.length
+            ? priceElement.text().trim().replace(/\s+/g, " ")
+            : "0";
+          const price =
+            parseFloat(priceText.replace(/[^\d,]/g, "").replace(",", ".")) || 0;
 
-        const image = imageElement
-          ? imageElement.getAttribute("data-src")
-          : "No image";
-        const specs = {};
+          const image = imageElement.length
+            ? imageElement.attr("data-src")
+            : "No image";
+          const specs = {};
 
-        productElement
-          .querySelectorAll(".productlist_spec ul li p")
-          .forEach((specElement) => {
-            const specNameElement = specElement.querySelector("#specname");
-            const specValueElement = specElement.querySelector("#specvalue");
-            const specName = specNameElement
-              ? specNameElement.textContent.trim()
-              : "";
-            const specValue = specValueElement
-              ? specValueElement.textContent.trim()
-              : "";
+          $(productElement)
+            .find(".productlist_spec ul li p")
+            .each((_, specElement) => {
+              const specNameElement = $(specElement).find("#specname");
+              const specValueElement = $(specElement).find("#specvalue");
+              const specName = specNameElement.length
+                ? specNameElement.text().trim()
+                : "";
+              const specValue = specValueElement.length
+                ? specValueElement.text().trim()
+                : "";
 
-            if (specName.includes("İşlemci Numarası")) {
-              specs["CPU"] = specValue;
-            } else if (specName.includes("Grafik İşlemci")) {
-              specs["GPU"] = specValue;
-            } else if (specName.includes("Anakart Chipseti")) {
-              specs["Motherboard"] = specValue;
-            } else if (specName.includes("Ram (Sistem Belleği)")) {
-              specs["Ram"] = specValue;
-            }
-          });
+              if (specName.includes("İşlemci Numarası")) {
+                specs["CPU"] = specValue;
+              } else if (specName.includes("Grafik İşlemci")) {
+                specs["GPU"] = specValue;
+              } else if (specName.includes("Anakart Chipseti")) {
+                specs["Motherboard"] = specValue;
+              } else if (specName.includes("Ram (Sistem Belleği)")) {
+                specs["Ram"] = specValue;
+              }
+            });
 
-        return { name, price, image, link, specs, store: "vatan" };
-      });
-    });
+          return { name, price, image, link, specs, store: "vatan" };
+        }
+      );
 
-    products.push(...productsOnPage);
+      products.push(...productsOnPage);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
   }
 
   return products;
@@ -119,11 +148,7 @@ router.get("/", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: [
-        `--proxy-server=${proxyAddress}`,
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-      ],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
 
@@ -145,10 +170,11 @@ router.get("/", async (req, res) => {
     await page.setGeolocation({ latitude: 41.0082, longitude: 28.9784 });
     await page.emulateTimezone("Europe/Istanbul");
 
-    const totalPages = await getTotalPages(page, baseUrl);
+    /* const totalPages = await getTotalPages(page, baseUrl);
+    console.log(totalPages) */
+    const totalPages = 6;
     const urls = generateUrls(baseUrl, totalPages);
-    const products = await fetchAllProducts(page, urls);
-
+    const products = await fetchAllProducts(urls);
     await browser.close();
 
     // MongoDB connection
